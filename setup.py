6cb7782c0d23cd352042fc91de0c979f8d8fde52
Jorge Guerra <jfguerra00@gmail.com>
2019-11-11 22:20:31 -0300

    Remove some install requires

--- setup.py
+++ setup.py
@@ -30,8 +30,7 @@ setup(
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
-        'setuptools',
-        'Django>=1.8,<1.9',
+        'Django>=1.8',
     ],
     test_suite='django_admin_bootstrapped.runtests.runtests',
     packages=find_packages(),
